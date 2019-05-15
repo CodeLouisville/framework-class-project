03d6e0df1fb5dfc4e77445d1630735edc96c3912
Dvontre Coleman <dvontrec@gmail.com>
2019-05-15 18:35:07 -0400

    Create OOP code example

--- src/App.js
+++ src/App.js
@@ -1,8 +1,48 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  constructor () {
+    super();
+    this.state = {
+      greeting: 'We did it!',
+      people: [
+        { name: 'Alex', age: 21 },
+        { name: 'Ben', age: 18 },
+        { name: 'Abe', age: 31 }
+      ]
+    };
+  }
+
+  getOldPeople () {
+    let people = this.state.people;
+    let olds = [];
+    for (let i = 0; i < people.length; i++) {
+      if (people[i].age > 18) {
+        olds.push(people[i]);
+      }
+    }
+    return olds;
+  }
+
+  getYoungest () {
+    let people = this.state.people;
+    let youngest = people[0];
+    for (let i = 0; i < people.length; i++) {
+      if (people[i].age < youngest.age) {
+        youngest = people[i];
+      }
+    }
+    return (
+      <div>
+        <h1>{youngest.name}</h1>
+        <p>{youngest.age}</p>
+      </div>
+    );
+  }
+
   render () {
-    return <div className='App'>{/*  Say Hello World Here */}</div>;
+    console.log(this.getOldPeople());
+    return <div>{this.getYoungest()}</div>;
   }
 }
 
