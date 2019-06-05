00a3a9d900faf74042451efc12e623d64cc1bcea
Dvontre Coleman <dvontrec@gmail.com>
2019-06-04 22:00:53 -0400

    Update src/Person.js
    Co-Authored-By: Ryan Hartlage <ryanplusplus@gmail.com>

--- src/Person.js
+++ src/Person.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Person = props => {
   return (
     <div key={props.index}>
-      <h2>{props.person.name} <span onClick={() => { props.deletePerson(props.index); }}>delete</span></h2>
+      <h2>{props.person.name} <span onClick={() => props.deletePerson(props.index)}>delete</span></h2>
       <p>{props.person.age}</p>
     </div>
   );
